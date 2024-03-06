22286e0689ed019dd2ac6816ce315d3aa6565ec2
sadikshak <kaflesadiksha@gmail.com>
2024-03-05 20:44:28 -0700

    created logic for gametictactoe

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("welcome to Tic Tac Toe")
+let turn = "X" 
+//initial variable x 
+
+//Function to change the turn 
+const changeTurn =()=>{
+return turn=== "X" ? "0" : "X"
+}
+
+//function to check win 
+
+const checkWin = ()=>{
+
+}
+//Game logic 
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element =>
+{
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click',()=>{
+
+    })
+}
