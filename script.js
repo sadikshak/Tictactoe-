8cc0d76762588fde4911037fa46e4b8599c03034
sadikshak <kaflesadiksha@gmail.com>
2024-03-11 22:02:23 -0600

    created  query foe boxtext and styled image

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 console.log("welcome to Tic Tac Toe")
 let turn = "X" 
 //initial variable x 
+let gameover= false;
 
 //Function to change the turn 
 const changeTurn =()=>{
@@ -10,7 +11,26 @@ return turn=== "X" ? "0" : "X"
 //function to check win 
 
 const checkWin = ()=>{
-
+    let boxtext =document.getElementsByClassName('boxtext')
+    let wins= [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [2,4,6],
+        [0,4,8],
+    ]
+wins.forEach(e=>
+    {
+if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[1]].innerText === boxtext [e[2]].innerText) && (boxtext [e[0]].innerText !== ""))
+    {
+        document.querySelector('.info').innerText = boxtext[e[0]].innerText + "Won";
+        gameover=true;
+        document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width= "200px"
+    }
+})
 }
 //Game logic 
 let boxes = document.getElementsByClassName("box");
@@ -19,6 +39,13 @@ Array.from(boxes).forEach(element =>
     let boxtext = element.querySelector('.boxtext');
     element.addEventListener('click',()=>{
 if(boxtext.innerText === ''){
+    boxtext.innerText = turn;
+    turn=changeTurn();
+    checkWin();
+    if(!gameover)
+    {
+        document.getElementsByClassName("info") [0] .innerText = "Turn for "  + turn;
+    }
     
 }
     })
